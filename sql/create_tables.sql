126a1536638c4661c745397798e9da5a9acf69b3
Thomas Tontchev <rtonttu@gmail.com>
2017-09-14 12:57:39 +0300

    Muokatut SQL-tiedostot tehty

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,18 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Singer(
+id SERIAL PRIMARY KEY,
+name varchar(50) NOT NULL,
+password varchar(50) NOT Null
+);
+
+CREATE TABLE Concert(
+  id SERIAL PRIMARY KEY,
+  joulu INTEGER REFERENCES Joulu(id), -- Viiteavain Player-tauluun
+  name varchar(50) NOT NULL,
+  date DATE,
+  description varchar(400),
+  published DATE,
+  publisher varchar(50),
+  added DATE
+);
--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,1 +1,4 @@
 -- Lisää DROP TABLE lauseet tähän tiedostoon
+
+DROP TABLE IF EXISTS Singer CASCADE;
+DROP TABLE IF EXISTS Concert CASCADE;
