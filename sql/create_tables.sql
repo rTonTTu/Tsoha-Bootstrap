324550ecb86fdfb8043f1b1fe0cefa54e713f5d6
Thomas Tontchev <rtonttu@gmail.com>
2017-09-14 14:35:06 +0300

    SQL-käskyjen korjailua

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,16 +1,16 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
 
 CREATE TABLE Singer(
-id SERIAL PRIMARY KEY,
-name varchar(50) NOT NULL,
-password varchar(50) NOT Null
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  password varchar(50) NOT NULL
 );
 
 CREATE TABLE Concert(
   id SERIAL PRIMARY KEY,
-  joulu INTEGER REFERENCES Joulu(id), -- Viiteavain Player-tauluun
+  singer_id INTEGER REFERENCES Singer(id),
   name varchar(50) NOT NULL,
-  date DATE,
+  performing_date DATE,
   description varchar(400),
   published DATE,
   publisher varchar(50),
--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,4 +1,2 @@
--- Lisää DROP TABLE lauseet tähän tiedostoon
-
 DROP TABLE IF EXISTS Singer CASCADE;
 DROP TABLE IF EXISTS Concert CASCADE;
